51c0d757b5e904352b38556ad2b25c8e4f2adfb8
raviaws523 <raviaws523@gmail.com>
2023-04-07 16:17:02 +0530

    push

---
+++ src/components/Navbar.vue
@@ -0,0 +1,531 @@
+<template>
+    <div class="conatiner-fluid">
+<div class="row">
+    <div class="col-lg-3">
+        <div class='dashboard'>
+    <div class="dashboard-nav">
+        <header>
+            <a href="#" class="brand-logo"><span>MY BRAND</span></a></header>
+            <nav class="dashboard-nav-list">
+            <a href="#" class="dashboard-nav-item active"><i class="fa fa-home"></i> Home</a>
+            <a href="#" class="dashboard-nav-item"><i class="fa fa-hashtag"></i> Exploare </a>
+            <a href="#" class="dashboard-nav-item"><i class="fa fa-bell"></i> Notification </a>
+            <a href="#" class="dashboard-nav-item"><i class="fa fa-envelope"></i> Message </a>
+            <a href="#" class="dashboard-nav-item"><i class="fa fa-bookmark"></i> Bookmark </a>
+            <a href="#" class="dashboard-nav-item"><i class="fa fa-list"></i> List </a>
+            <a href="#" class="dashboard-nav-item"><i class="fa fa-user"></i> Profile </a>
+            <div class="mt-1 mb-1">
+                <a class="buttons text-white" href="#myModal"  data-toggle="modal" >Tweet</a>
+            </div>
+            <a href="#" class="dashboard-nav-item"><i class="fa fa-power-off"></i> Logout </a>
+              
+
+        </nav>
+    </div>
+</div>
+    </div>
+</div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog border-0">
+      <div class="modal-content border-0">
+        <div class="modal-body border-0">
+            
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+            <div class="row tweet">
+        <div class="col-lg-1">
+            <div class="tweet-image">
+                <img src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" alt="">
+            </div>
+        </div>
+        <div class="col-lg-11">
+           <div class="tweet-post">
+            <div class="form-group">
+                <select class="form-control" id="exampleFormControlSelect1">
+                <option>Everyone</option>
+                <option>Friends</option>
+                <option>Private</option>
+                </select>
+            </div>
+            <textarea
+        maxlength="280"
+        name=""
+        id=""
+        cols="28"
+        rows="100"
+        placeholder="what happening?"
+        style="    width: 410px;margin-left: -151px;"
+      ></textarea>
+           </div>
+        </div>
+        </div>
+       
+        <div class="social-menu p-3">
+          <div class="text-left ml-5" style="margin-bottom:-15px;">
+            <p class="ml-2" style="color:#1d9bf0;font-size:14px;"><i class="fa fa-globe"></i> Everyone can reply</p>
+          </div>
+        <ul>
+            <li><a ><i class="fa fa-picture-o"></i></a></li>
+            <li><a ><i class="fa fa-video-camera"></i></a></li>
+            <li><a ><i class="fa fa-file-text"></i></a></li>
+            <li><a ><i class="fa fa-calendar"></i></a></li>
+            <li><a ><i class="fa fa-map-marker"></i></a></li>
+            <li><a ><i class="fa fa-tag"></i></a></li>
+        </ul>
+        <div class="tweet-button">
+            <button>Tweet</button>
+        </div>
+    </div>
+        </div>
+       
+      </div>
+    </div>
+  </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+nav {
+    display: block;
+}
+
+body {
+    margin: 0;
+    font-family: "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #515151;
+    text-align: left;
+    background-color: #e9edf4;
+}
+.buttons {
+  background-color: #1d9bf0;
+  border: none;
+  color: white;
+  padding: 12px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  width: 161px;
+  border-radius: 35px;
+}
+h1, h2, h3, h4, h5, h6 {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+}
+
+p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+}
+
+a {
+    color: #3f84fc;
+    text-decoration: none;
+    background-color: transparent;
+}
+
+a:hover {
+    color: #0458eb;
+    text-decoration: underline;
+}
+
+h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
+    font-family: "Nunito", sans-serif;
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    line-height: 1.2;
+}
+
+h1, .h1 {
+    font-size: 2.5rem;
+    font-weight: normal;
+}
+
+.card {
+    position: relative;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    border-radius: 0;
+}
+
+.card-body {
+    -webkit-box-flex: 1;
+    -webkit-flex: 1 1 auto;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    padding: 1.25rem;
+}
+
+.card-header {
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 0;
+    background-color: rgba(0, 0, 0, 0.03);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+    text-align: center;
+}
+
+.dashboard {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    min-height: 100vh;
+}
+
+.dashboard-app {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -webkit-box-flex: 2;
+    -webkit-flex-grow: 2;
+    -ms-flex-positive: 2;
+    flex-grow: 2;
+    margin-top: 84px;
+}
+
+.dashboard-content {
+    -webkit-box-flex: 2;
+    -webkit-flex-grow: 2;
+    -ms-flex-positive: 2;
+    flex-grow: 2;
+    padding: 25px;
+}
+
+.dashboard-nav {
+    min-width: 238px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    overflow: auto;
+    background-color: #fff;
+}
+
+.dashboard-compact .dashboard-nav {
+    display: none;
+}
+
+.dashboard-nav header {
+    min-height: 84px;
+    padding: 8px 27px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+
+.dashboard-nav header .menu-toggle {
+    display: none;
+    margin-right: auto;
+}
+
+.dashboard-nav a {
+    color: #515151;
+}
+
+.dashboard-nav a:hover {
+    text-decoration: none;
+}
+
+.dashboard-nav {
+    background-color: #fff;
+}
+
+.dashboard-nav a {
+    color: black;
+    font-size: 16px;
+}
+.dashboard-nav a .active{
+    color: black;
+    font-size: 16px;
+    font-weight: 600;
+}
+
+.brand-logo {
+    font-family: "Nunito", sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    color: #515151;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+
+.brand-logo:focus, .brand-logo:active, .brand-logo:hover {
+    color: #dbdbdb;
+    text-decoration: none;
+}
+
+.brand-logo i {
+    color: #d2d1d1;
+    font-size: 27px;
+    margin-right: 10px;
+}
+
+.dashboard-nav-list {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+}
+
+.dashboard-nav-item {
+    min-height: 56px;
+    padding: 8px 20px 8px 70px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    letter-spacing: 0.02em;
+    transition: ease-out 0.5s;
+}
+
+.dashboard-nav-item i {
+    width: 36px;
+    font-size: 19px;
+    margin-left: -40px;
+}
+
+.dashboard-nav-item:hover {
+    background: rgba(255, 255, 255, 0.04);
+}
+
+.active {
+    color: black;
+    font-weight: 600;
+}
+
+.dashboard-nav-dropdown {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+}
+
+.dashboard-nav-dropdown.show {
+    background: rgba(255, 255, 255, 0.04);
+}
+
+.dashboard-nav-dropdown.show > .dashboard-nav-dropdown-toggle {
+    font-weight: bold;
+}
+
+.dashboard-nav-dropdown.show > .dashboard-nav-dropdown-toggle:after {
+    -webkit-transform: none;
+    -o-transform: none;
+    transform: none;
+}
+
+.dashboard-nav-dropdown.show > .dashboard-nav-dropdown-menu {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+}
+
+.dashboard-nav-dropdown-toggle:after {
+    content: "";
+    margin-left: auto;
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 5px solid rgba(81, 81, 81, 0.8);
+    -webkit-transform: rotate(90deg);
+    -o-transform: rotate(90deg);
+    transform: rotate(90deg);
+}
+
+.dashboard-nav .dashboard-nav-dropdown-toggle:after {
+    border-top-color: rgba(255, 255, 255, 0.72);
+}
+
+.dashboard-nav-dropdown-menu {
+    display: none;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+}
+
+.dashboard-nav-dropdown-item {
+    min-height: 40px;
+    padding: 8px 20px 8px 70px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    transition: ease-out 0.5s;
+}
+
+.dashboard-nav-dropdown-item:hover {
+    background: rgba(255, 255, 255, 0.04);
+}
+
+.menu-toggle {
+    position: relative;
+    width: 42px;
+    height: 42px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    color: #443ea2;
+}
+
+.menu-toggle:hover, .menu-toggle:active, .menu-toggle:focus {
+    text-decoration: none;
+    color: #875de5;
+}
+
+.menu-toggle i {
+    font-size: 20px;
+}
+
+.dashboard-toolbar {
+    min-height: 84px;
+    background-color: #dfdfdf;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    padding: 8px 27px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 1000;
+}
+
+.nav-item-divider {
+    height: 1px;
+    margin: 1rem 0;
+    overflow: hidden;
+    background-color: rgba(236, 238, 239, 0.3);
+}
+
+@media (min-width: 992px) {
+    .dashboard-app {
+        margin-left: 238px;
+    }
+
+    .dashboard-compact .dashboard-app {
+        margin-left: 0;
+    }
+}
+
+
+@media (max-width: 768px) {
+    .dashboard-content {
+        padding: 15px 0px;
+    }
+}
+
+@media (max-width: 992px) {
+    .dashboard-nav {
+        display: none;
+        position: fixed;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        z-index: 1070;
+    }
+
+    .dashboard-nav.mobile-show {
+        display: block;
+    }
+}
+
+@media (max-width: 992px) {
+    .dashboard-nav header .menu-toggle {
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+    }
+}
+
+@media (min-width: 992px) {
+    .dashboard-toolbar {
+        left: 238px;
+    }
+
+    .dashboard-compact .dashboard-toolbar {
+        left: 0;
+    }
+}
+</style>
---
+++ src/config/energy_form_blank2.js
@@ -0,0 +1,161 @@
+var formData = {
+  forminfo: {
+    filled_table: "",
+    commande_devis: "DEVIS",
+    nom: "",
+    prenome: "",
+    dob: "",
+    email: "",
+    address: "",
+    code_postal: "",
+    ville: "",
+    agence: "Puget-sur-Argens",
+    add_if_diff: "",
+    tel_dom: "",
+    tel_port: "",
+    chauffage_existant: "",
+    version: 0,
+    form_num: ""
+  },
+  hydrofugation: {
+    type: "",
+    type_maison: "",
+    color: "",
+    price: null,
+    surface: null,
+    total_price: null,
+    options: "",
+    description: "",
+    type_autre: "",
+    type_maison_autre: ""
+  },
+
+  facade: {
+    type: "",
+    type_maison: "",
+    surface: "",
+    price: "",
+    total_price: "",
+    options: "",
+    description: "",
+    type_maison_autre: ""
+  },
+
+  peinture_di: {
+    type: "",
+    color: "",
+    surface: "",
+    price: "",
+    total_price: "",
+    description: ""
+  },
+
+  traitment_drc: {
+    designation: "",
+    entrax: "",
+    linear_meter: "",
+    price: "",
+    total_price: "",
+    description: ""
+  },
+
+  isolation_pl: {
+    designation: "",
+    color: "",
+    price: "",
+    quantity: "",
+    total_price: "",
+    description: ""
+  },
+
+  travaux_sous_t: {
+    designation: "",
+    epi: "",
+    price: "",
+    quantity: "",
+    total_price: "",
+    description: ""
+  },
+
+  travaux_sur_t: {
+    designation: "",
+    epi: "",
+    price: "",
+    quantity: "",
+    total_price: "",
+    description: "",
+    file_type: "",
+    file_color: "",
+    surface_total: "",
+    photo: ""
+  },
+
+  traitment_de_c: {
+    designation: "",
+    autre: "",
+    price: "",
+    quantity: "",
+    total_price: "",
+    description: ""
+  },
+
+  chauffage: {
+    piece: "",
+    volume: "",
+    designation: "",
+    quantity: "",
+    price: "",
+    total_price: "",
+    description: ""
+  },
+
+  isolation_dc: {
+    type: "",
+    type_maison: "",
+    designation: " ",
+    pouvoir: "",
+    surface: "",
+    price: "",
+    total_price: "",
+    description: ""
+  },
+
+  autre: {
+    designation: "",
+    price: "",
+    quantity: "",
+    total_price: "",
+    description: "",
+    provisional: "",
+    attest_1: "",
+    attest_2: "",
+    attest_3: "",
+    signature: ""
+  },
+
+  reglement: {
+    comptant: "",
+    comptant_1: "",
+    comptant_2: "",
+    finance: "",
+    finance_1: "",
+    finance_2: "",
+    finance_3: "",
+    months: "",
+    monthly_amount_insurance: "",
+    monthly_amount_wo_insurance: "",
+    taeg: "",
+    debitor_rate: "",
+    funding_org: "",
+    total_ht: "",
+    tva: "5.5",
+    total_ttc: "",
+    name_1: "",
+    name_2: "",
+    made_in: "",
+    date: "",
+    signature: ""
+  }
+};
+
+export default formData;
